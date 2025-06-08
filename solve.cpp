41ecddb93b6768918f23af9f20c89d9fc84639ac
Akash <akashudsouza12@gmail.com>
2025-06-09 00:46:50 +0530

    initial commit

---
+++ solve.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+// clang-format off
+// *INDENT-OFF*
+
+/* ---------- pbds ordered set ---------- */
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds; typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+/* ---------- overflow ---------- */
+// #define ll long long
+// #define MOD 1'000'000'007
+
+/* ---------- binpow ---------- */
+// int binpow(int a, int b) { if (b == 0) { return 1; } if (b & 1) { return (1LL * a * binpow(a, b - 1)) % MOD; } else { return binpow((1LL * a * a) % MOD, b / 2) % MOD; }}
+
+/* ---------- modular multiplicative inverse ---------- */
+// int inverse(int x) { return binpow(x, MOD - 2);}
+
+/* ---------- basic computation ---------- */
+// int ceil2(int a, int b) { return a ? ( (a - 1) / b + 1 ) : 0; }
+
+// *INDENT-ON*
+// clang-format on
+
+void solve() {
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    // int _t; cin >> _t; while (_t--)
+    solve();
+}
