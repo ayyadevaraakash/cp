4b1976d19f1c5c2aa3637b939edaf702d501c1b2
Akash <akashudsouza12@gmail.com>
2025-06-16 15:48:13 +0530

    new update

--- solve.cpp
+++ solve.cpp
@@ -33,8 +33,10 @@ signed main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
+    // clang-format off
     // int _t; cin >> _t; while (_t--)
     solve();
+    // clang-format on
 
     return 0;
 }
