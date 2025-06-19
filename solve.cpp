6c45471affab0c9486cd2948bf976da9ad75ec9e
Akash <akashudsouza12@gmail.com>
2025-06-19 17:47:05 +0530

    new update

--- solve.cpp
+++ solve.cpp
@@ -10,12 +10,16 @@ using namespace std;
 // #include <ext/pb_ds/tree_policy.hpp>
 // using namespace __gnu_pbds; typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 
-/* ---------- overflow ---------- */
+/* ---------- overflow and data types ---------- */
 // #define ll long long
 // #define MOD 1'000'000'007
+// #define int long long
 
 /* ---------- binpow ---------- */
 // int binpow(int a, int b) { if (b == 0) { return 1; } if (b & 1) { return (1LL * a * binpow(a, b - 1)) % MOD; } else { return binpow((1LL * a * a) % MOD, b / 2) % MOD; }}
+
+/* ---------- binpow without MOD ---------- */
+// long long binpow(long long a, long long b) { if (b == 0) { return 1; } if (b & 1) { return (a * binpow(a, b - 1)); } else { return binpow((a * a), b / 2); }}
 
 /* ---------- modular multiplicative inverse ---------- */
 // int inverse(int x) { return binpow(x, MOD - 2);}
